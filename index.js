30d79776db6a6c160071f62b65aaed3d40b001c7
RafaelPreziaGomes <rafafprezia@gmail.com>
2022-07-13 15:28:53 -0400

    finished working logic of the application

---
+++ index.js
@@ -0,0 +1,435 @@
+import inquirer from "inquirer";
+import fs from "fs";
+
+import generateTeamPage from "./src/page-template.js";
+
+import Manager from "./lib/Manager.js";
+
+import Engineer from "./lib/Engineer.js";
+
+import Intern from "./lib/Intern.js";
+
+import http from "http";
+//  create a function which promps the user to log in or create an account
+const loginPrompt = () => {
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "login",
+      message: "Would you like to log in or create an account?",
+      choices: ["Log in", "Create an account"],
+    },
+  ]);
+};
+
+//  if user selects to log in, prompt user for username and password
+const loginPrompt2 = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is your username?",
+    },
+    {
+      type: "password",
+      name: "password",
+      message: "What is your password?",
+    },
+  ]);
+};
+
+//  if user selects to create an account, prompt user for username and password
+const loginPrompt3 = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is your username?",
+    },
+
+    {
+      type: "password",
+      name: "password",
+      message: "What is your password?",
+    },
+    {
+      type: "password",
+      name: "password2",
+      message: "Confirm your password",
+    },
+  ]);
+  // save this username to a variable
+  const username = answers.username;
+
+  // save this password to a variable
+  const password = answers.password;
+};
+
+//  create a function to create a file with the user's username and password
+const createFile = (username, password) => {
+  fs.writeFile(
+    `./users/${username}.json`,
+    `{
+      "username":"${username}",
+      "password": "${password}",
+      "file-location": "dist/index.html"
+    } `,
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+};
+
+//  create a function that validates if user's username and password are correct
+const validateUser = (username, password) => {
+  return fs.readFile(`./users/${username}.json`, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const user = JSON.parse(data);
+      if (user.password === password) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  });
+};
+
+// if user logs in successfully
+
+//  load user website
+const loadWebsite = () => {
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "load",
+      message: "Would you like to load your website?",
+      choices: ["Yes", "No"],
+    },
+  ]);
+};
+// if user selects to load website
+// load user website
+const loadWebsite2 = () => {
+  const userWebsite = fs.readFile(`./users/${username}.json`, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const user = JSON.parse(data);
+      return user.website;
+    }
+  });
+};
+//  respond with the json data of the user's website
+const loadWebsite3 = () => {
+  return userWebsite;
+};
+// if user selects to not load website
+// respond with a message that the user has been logged out
+const loadWebsite4 = () => {
+  return "You have been logged out";
+};
+
+//  if the user decides to create a new account
+//
+
+const team = [];
+
+const getManager = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter manager name (required):",
+        validate: (managerInput) => {
+          if (managerInput) {
+            return true;
+          } else {
+            console.log("Please enter the manager's name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter manager ID (required):",
+        validate: (managerInput) => {
+          if (managerInput) {
+            return true;
+          } else {
+            console.log("Please enter the manager's ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter manager email (required):",
+        validate: (managerInput) => {
+          if (managerInput) {
+            return true;
+          } else {
+            console.log("Please enter the manager's email!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "office",
+        message: "Enter manager office number (required):",
+        validate: (managerInput) => {
+          if (managerInput) {
+            return true;
+          } else {
+            console.log("Please enter the manager's office number!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((data) => {
+      //console.log(data);
+      const manager = new Manager(data.name, data.id, data.email, data.office);
+      team.push(manager);
+      userMenu();
+    });
+};
+
+const userMenu = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "What would you like to do?",
+        choices: ["add an Engineer", "add an Intern", "finish building team"],
+      },
+    ])
+    .then((menuSelection) => {
+      switch (menuSelection.menu) {
+        case "add an Engineer":
+          console.log("Add Engineer");
+          getEngineer();
+          break;
+
+        case "add an Intern":
+          console.log("Add Intern");
+          getIntern();
+          break;
+
+        case "finish building team":
+          console.log("Team Completed");
+          buildTeam();
+          break;
+      }
+    });
+};
+
+const getEngineer = () => {
+  console.log(`
+*******************
+* Adding Engineer *
+*******************
+    `);
+
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Engineer's name (required):",
+        validate: (engineerInput) => {
+          if (engineerInput) {
+            return true;
+          } else {
+            console.log("Please enter the engineer's name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Engineer's ID (required):",
+        validate: (engineerInput) => {
+          if (engineerInput) {
+            return true;
+          } else {
+            console.log("Please enter the engineer's ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Engineer's email (required):",
+        validate: (engineerInput) => {
+          if (engineerInput) {
+            return true;
+          } else {
+            console.log("Please enter the engineer's email!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "githubName",
+        message: "Engineer's GitHub Username (required):",
+        validate: (engineerInput) => {
+          if (engineerInput) {
+            return true;
+          } else {
+            console.log("Please enter the engineer's GitHub Username!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((data) => {
+      //console.log(data);
+      const engineer = new Engineer(
+        data.name,
+        data.id,
+        data.email,
+        data.githubName
+      );
+      team.push(engineer);
+      userMenu();
+    });
+};
+
+const getIntern = () => {
+  console.log(`
+*******************
+*  Adding Intern  *
+*******************
+    `);
+
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Intern's name (required):",
+        validate: (internInput) => {
+          if (internInput) {
+            return true;
+          } else {
+            console.log("Please enter the intern's name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Intern's ID (required):",
+        validate: (internInput) => {
+          if (internInput) {
+            return true;
+          } else {
+            console.log("Please enter the intern's ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Intern's email (required):",
+        validate: (internInput) => {
+          if (internInput) {
+            return true;
+          } else {
+            console.log("Please enter the intern's email!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Intern's school (required):",
+        validate: (internInput) => {
+          if (internInput) {
+            return true;
+          } else {
+            console.log("Please enter the intern's school!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((data) => {
+      //console.log(data);
+      const intern = new Intern(data.name, data.id, data.email, data.school);
+      team.push(intern);
+      userMenu();
+    });
+};
+
+const buildTeam = () => {
+  console.log(`
+**********************
+* Building Team Page *
+**********************
+    `);
+
+  fs.writeFile("dist/index.html", generateTeamPage(team), (err) => {
+    if (err) throw err;
+    console.log("Team page created successfully");
+
+    //  create a http server to serve file form dist folder
+    const server = http.createServer((req, res) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(fs.readFileSync("dist/index.html"));
+    });
+    server.listen(3000, () => {
+      console.log(`Server is listening on port 3000`);
+    });
+  });
+};
+
+function init() {
+  // loginPrompt().then((answers) => {
+  //   console.log(answers);
+  //   if (answers.login == "Log in") {
+  //     loginPrompt2().then((answers) => {
+  //       if ((answers.username, answers.password)) {
+  //         if (validateUser(answers.username, answers.password) === true) {
+  //           console.log("Welcome back!");
+  //           loadWebsite().then(loadWebsite2().then(loadWebsite3()));
+  //         }
+  //         if (validateUser(answers.username, answers.password) === false)
+  //           if (err) {
+  //             //  if error
+  //             console.log("Error there is a problem finding your account");
+  //           }
+
+  //         {
+  //           console.log("Invalid username or password");
+  //         }
+  //       }
+  //     });
+  //   } else {
+  loginPrompt3().then((answers) => {
+    if (answers.username && answers.password) {
+      // create file
+      createFile(answers.username, answers.password);
+    }
+
+    //  if file exists
+    if (answers.username && answers.password) getManager();
+  });
+}
+
+init();
---
+++ src/page-template.js
@@ -0,0 +1,146 @@
+var bodyHTML = "";
+export default function generateTeam(team) {
+  console.log(team[0].name);
+  const role = team[0].getRole();
+  console.log(`Role: ${role}`);
+
+  for (var i = 0; i < team.length; i++) {
+    const role = team[i].getRole();
+
+    switch (role) {
+      case "Manager":
+        generateManager(team[i]);
+        break;
+
+      case "Engineer":
+        generateEngineer(team[i]);
+        break;
+
+      case "Intern":
+        generateIntern(team[i]);
+        break;
+    }
+  }
+
+  var htmlTemplate = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <style>
+    * {
+    font-family: ubuntu;
+  }
+  
+  .bg-blue {
+    background-color: rgb(243, 243, 243);
+    color: rgb(2, 0, 0);
+  }
+  
+  .card {
+    display: inline-block;
+    width: 100%;
+    margin: 10px 0;
+  }
+  
+  h1 {
+    border: 3px solid #007bff;
+    background-color: #007bff;
+    border-radius: 5px;
+    margin: 10px;
+    color: white;
+  }
+  </style>
+    <body>
+    <div class="container">
+        <div class="row">    
+            <div class="col mx-auto">
+                <h1 class="text-center">My Team</h1>
+    
+                <div class="card-deck">
+                    ${bodyHTML}
+                </div>
+            </div>
+        </div>
+    </div>    
+    </body>
+    </html>
+    `;
+
+  return htmlTemplate;
+}
+
+function generateManager(manager) {
+  var managerHTML = `
+    <div class="card">
+        
+        <div class="card-title bg-blue p-3">
+            ${manager.name} - ID: ${manager.id}
+        
+            <div class="card-subtitle p-3">
+                Manager
+            </div>
+        </div>
+        
+        <div class="card-body">
+            <p><i class="fa-solid fa-envelope"></i> | <a href="mailto:${manager.email}">${manager.email}</a></p>
+            <p><i class="fa-solid fa-building-columns"></i> | ${manager.office}</p>
+        </div>
+    </div>
+    `;
+
+  bodyHTML += managerHTML;
+  return bodyHTML;
+}
+
+function generateEngineer(engineer) {
+  var engineerHTML = `
+    <div class="card">
+        
+    <div class="card-title bg-blue p-3">
+            ${engineer.name} - ID: ${engineer.id}
+        
+            <div class="card-subtitle p-3">
+                Engineer
+            </div>
+        </div>
+        <div class="card-body">
+            <p><i class="fa-solid fa-envelope"></i> | <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+            <p><i class="fa-brands fa-github-square"></i> | <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></p>
+        </div>
+    </div>
+    `;
+
+  bodyHTML += engineerHTML;
+  return bodyHTML;
+}
+
+function generateIntern(intern) {
+  var internHTML = `
+    <div class="card">
+        
+    <div class="card-title bg-blue p-3">
+            ${intern.name} - ID: ${intern.id}
+        
+            <div class="card-subtitle p-3">
+                Intern
+            </div>
+        </div>
+        <div class="card-body">
+            <p><i class="fa-solid fa-envelope"></i> | <a href="mailto:${intern.email}">${intern.email}</a></p>
+            <p><i class="fa-solid fa-graduation-cap"></i> | ${intern.school}</p>
+        </div>
+    </div>
+    `;
+
+  bodyHTML += internHTML;
+  return bodyHTML;
+}
